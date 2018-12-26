a9b49a0618a3d6822508908e9f79ecaed0088c06
Halkin David <northfire@maail.ru>
2018-12-26 16:49:11 +0200

    3 pages

--- app/js/main.js
+++ app/js/main.js
@@ -18,3 +18,17 @@ $(document).ready(function(){
         return false;
      });
 });
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {        // If page is scrolled more than 50px
+        $('#return_to_top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return_to_top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return_to_top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
